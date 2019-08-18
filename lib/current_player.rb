c459456445dc68056a19c4a414931da71cc967eb
Patrick Alexander <patrick_alexander@mail.com>
2019-08-18 04:53:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 counter = 0
 def turn_count(board)
   board.each do |board|
-    if #{board} == " " \\ ""
-    return #{counter} 
-  counter += 1 
+    
   end
 end
 
